57da234f3727b54f04030828ce45ce67830df6cb
janibek <janibekabdurakhimov357@gmail.com>
2025-12-23 11:43:15 +0500

    Validation

--- src/ui/ConsoleMenu.java
+++ src/ui/ConsoleMenu.java
@@ -1,8 +1,13 @@
 package ui;
 
+import core.Bank;
+import core.BankService;
+import validation.PasswordValidation;
 import java.util.Scanner;
 
 public class ConsoleMenu {
+    private static final BankService service = new BankService();
+    static PasswordValidation validate = new PasswordValidation();
     static Scanner in = new Scanner(System.in);
     public void start() {
         int choice;
@@ -43,6 +48,7 @@ public class ConsoleMenu {
         String name = in.nextLine();
         System.out.println("Phone number: ");
         int phoneNumber;
+
         while (true) {
             phoneNumber = inputInt("+998 ");
             if (String.valueOf(Math.abs(phoneNumber)).length() != 9) {
@@ -53,6 +59,21 @@ public class ConsoleMenu {
             }
         }
 
+        System.out.println("Enter the password (a-z, 0-9):");
+        String password;
+
+        while (true) {
+            password = in.nextLine();
+            if (validate.hasDigit(password) && validate.checkLength(password) && validate.hasCharacter(password)) {
+                break;
+            } else {
+                validate.errorShow(password);
+            }
+        }
+
+        service.register(name, phoneNumber, password);
     }
+
+
 
 }
---
+++ src/validation/PasswordValidation.java
@@ -0,0 +1,38 @@
+package validation;
+
+public class PasswordValidation {
+    public boolean hasDigit(String password) {
+        int count = 0;
+        for (int i = 0; i  < password.length(); i++) {
+            char c = password.charAt(i);
+            if (Character.isDigit(c)) {
+                count++;
+            }
+        }
+        return count > 2;
+    }
+
+    public boolean hasCharacter(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            return Character.isAlphabetic(c);
+        }
+        return false;
+    }
+
+    public boolean checkLength(String password) {
+        return password.length() >= 8;
+    }
+
+    public void errorShow(String password) {
+        if (!hasDigit(password)) {
+            System.out.println("Password must contain at least two numbers");
+        }
+        if (!hasCharacter(password)) {
+            System.out.println("Password must contain characters!");
+        }
+        if (!checkLength(password)) {
+            System.out.println("Password length must be at least 8");
+        }
+    }
+}
